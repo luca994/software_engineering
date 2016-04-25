bb3485d23a272f8cf4993a42f6665264aa41f8ff
MaxGV <massimiliano.ventura@mail.polimi.it>
2016-04-25 17:47:27 +0200

    sistemata (non completamente) tesseracostruzione, aggiunto bonusriutilizzotesseracostruzioe

--- src/main/java/model/BonusRiutilizzoCostruzione.java
+++ src/main/java/model/BonusRiutilizzoCostruzione.java
@@ -3,17 +3,37 @@
  */
 package model;
 
+import java.util.Iterator;
+
 /**
  * @author Massimiliano Ventura
- *
+ *To make the method azioneBonus work you have to set the tile that have the bonus you 
+ *want to reuse with this bonus. Then you can call the method passing the player that 
+ *owns the previously set tile.
  */
 public class BonusRiutilizzoCostruzione extends Bonus {
 
-	private OggettoConBonus tessera;
+	private TesseraCostruzione tessera;
+	/**
+	 * @return the tessera
+	 */
+	public OggettoConBonus getTessera() {
+		return tessera;
+	}
+	/**
+	 * @param tessera the tessera to set
+	 */
+	public void setTessera(TesseraCostruzione tessera) {
+		this.tessera = tessera;
+	}
 	@Override
 	public void azioneBonus(Giocatore giocatore) {
-		// TODO Auto-generated method stub
-
+		Iterator<Bonus> itbonus=tessera.getElencoBonus().iterator();
+		while(itbonus.hasNext())
+		{
+			itbonus.next().azioneBonus(giocatore);
+		}
+		
 	}
 
 }
--- src/main/java/model/TesseraCostruzione.java
+++ src/main/java/model/TesseraCostruzione.java
@@ -5,4 +5,29 @@ import java.util.Set;
 public class TesseraCostruzione extends OggettoConBonus {
 
 	private Set<String> nomeCittà;
+	private Set<Bonus> elencoBonus;
+	/**
+	 * @return the nomeCittà
+	 */
+	public Set<String> getNomeCittà() {
+		return nomeCittà;
+	}
+	/**
+	 * @param nomeCittà the nomeCittà to set
+	 */
+	public void setNomeCittà(Set<String> nomeCittà) {
+		this.nomeCittà = nomeCittà;
+	}
+	/**
+	 * @return the elencoBonus
+	 */
+	public Set<Bonus> getElencoBonus() {
+		return this.elencoBonus;
+	}
+	/**
+	 * @param elencoBonus the elencoBonus to set
+	 */
+	public void setElencoBonus(Set<Bonus> elencoBonus) {
+		this.elencoBonus = elencoBonus;
+	}
 }
