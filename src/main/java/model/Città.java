d3a918e18d4f719dec8c2418883d6927ae9a506f
Riccardo Remigio <riccardoremigio@gmail.com>
2016-04-26 14:04:56 +0200

    risolti conflitti

--- src/main/java/model/Città.java
+++ src/main/java/model/Città.java
@@ -1,9 +1,5 @@
 package model;
 
-import java.util.Set;
-
-public class Città {
-=======
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
--- src/main/java/model/Giocatore.java
+++ src/main/java/model/Giocatore.java
@@ -2,29 +2,6 @@ package model;
 
 import java.util.List;
 
-public class Giocatore {
-
-		private int id;
-		private String nome;
-		private List<Emporio> emporiRimasti;
-		private List<Assistente> assistenti;
-		private List<CartaPolitica> cartePolitica;
-		private boolean azionePrincipale;
-		private boolean azioneOpzionale;
-		private String colore;
-		private List<OggettoConBonus> tessereUsate;
-		private List<OggettoConBonus> tessereValide;
-		private Azione azione;
-		private Gioco gioco;
-		private Tabellone tabellone;
-		private boolean connesso;
-		
-}
-=======
-package model;
-
-import java.util.List;
-
 /**
  * @author Luca
  *
