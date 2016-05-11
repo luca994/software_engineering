c1998d5e7c7105e4e2edb35c12bf13bd7f8403d8
MaxGV <massimiliano.ventura@mail.polimi.it>
2016-05-11 16:21:01 +0200

    bonus

---
+++ src/main/java/bonus/BonusAzionePrincipale.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package bonus;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import model.Giocatore;
+
+/**
+ * @author Massimiliano Ventura
+ *This bonus enables the Player to make another main move(Azione Principale)
+ */
+public class BonusAzionePrincipale implements Bonus {
+	private static final Logger log= Logger.getLogger( BonusAzionePrincipale.class.getName() );
+	/* (non-Javadoc)
+	 * @see model.Bonus#azioneBonus(model.Giocatore)
+	 */
+	@Override
+	public void azioneBonus(Giocatore giocatore) {
+		try{
+			if(giocatore==null)
+				throw new NullPointerException("Il giocatore non può essere nullo");	
+			giocatore.setAzionePrincipale(false);
+		}
+		catch(Exception e){
+			log.log( Level.WARNING,e.getLocalizedMessage(),e );
+		}
+	}
+}
