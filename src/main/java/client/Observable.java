a12daf1520d2f51138b69de05774e4fcfc74858a
MaxGV <massimiliano.ventura@mail.polimi.it>
2016-05-24 12:47:36 +0200

    aggiustati anche col codice: observer e observavble

---
+++ src/main/java/client/Observable.java
@@ -0,0 +1,33 @@
+package client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Observable {
+
+	private List<Observer> observers;
+	
+	public Observable(){
+		observers=new ArrayList<Observer>();
+	}
+	
+	public <T> void notificaObservers(T cambiamento,String[] input){
+		for(Observer o:observers){
+			o.update(cambiamento,input);
+		}
+	}
+	
+	public <T> void notificaObservers(T cambiamento){
+		for(Observer o:observers){
+			o.update(cambiamento);
+		}
+	}
+	
+	public void registerObserver(Observer osservatore){
+		observers.add(osservatore);
+	}
+	
+	public void removeObserver(Observer osservatore){
+		observers.remove(osservatore);
+	}	
+}
