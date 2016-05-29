72dee48f1f2b541ababaf16f2aecc93f3cf69f44
Luca Santini <luca2.santini@mail.polimi.it>
2016-05-29 21:04:18 +0200

    aggiunto test consigliere

---
+++ src/test/java/testModel/ConsigliereTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package testModel;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+
+import server.model.Consigliere;
+
+/**
+ * @author Luca
+ *
+ */
+public class ConsigliereTest {
+
+	/**
+	 * Test method for {@link server.model.Consigliere#Consigliere(java.awt.Color)}.
+	 */
+	@Test
+	public void testConsigliereNull() {
+		Consigliere consigliereTester = new Consigliere(null);
+		assertNotNull(consigliereTester);
+	}
+	
+	/**
+	 * Test method for {@link server.model.Consigliere#Consigliere(java.awt.Color)}.
+	 */
+	@Test
+	public void testConsigliereValido() {
+		Consigliere consigliereTester = new Consigliere(Color.darkGray);
+		assertNotNull(consigliereTester);
+	}
+
+	/**
+	 * Test method for {@link server.model.Consigliere#getColore()}.
+	 */
+	@Test
+	public void testGetColore() {
+		Consigliere consigliereTester = new Consigliere(Color.red);
+		assertEquals(Color.red, consigliereTester.getColore());
+	}
+
+	/**
+	 * Test method for {@link server.model.Consigliere#getColore()}.
+	 */
+	@Test
+	public void testGetColoreNull() {
+		Consigliere consigliereTester = new Consigliere(null);
+		assertNull(consigliereTester.getColore());
+	}
+}
