2bb3fb31c7c120bf966c6e7d86c819a94358e9c5
MaxGV <massimiliano.ventura@mail.polimi.it>
2016-06-20 10:26:23 +0200

    classe messaggio chat

---
+++ src/main/java/client/gui/MessaggioChat.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package client.gui;
+
+import java.io.Serializable;
+
+/**
+ * @author Massimiliano Ventura
+ *
+ */
+public class MessaggioChat implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6735858160492701007L;
+	private String autore;
+	private String msg;
+	/**
+	 * @param autore
+	 * @param msg
+	 */
+	public MessaggioChat(String autore, String msg) {
+		this.autore = autore;
+		this.msg = msg;
+	}
+	/**
+	 * @return the autore
+	 */
+	public String getAutore() {
+		return autore;
+	}
+	/**
+	 * @return the msg
+	 */
+	public String getMsg() {
+		return msg;
+	}
+	
+	
+}
